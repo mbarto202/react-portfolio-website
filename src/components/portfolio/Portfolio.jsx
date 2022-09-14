772a5b2ce410abf65cb49f2f0261629993d3dd46
mbarto202 <michael.d.barto@gmail.com>
2022-09-14 13:02:40 -0400

    started portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,17 @@ import './portfolio.css'
 const Portfolio = () => {
   return (
     <section id = 'portfolio'>
+      <h5>Most Recent Work</h5>
       <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+        <article className='portfolio_item'>
+          <div className='portfolio_img'></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/mbarto202"  className='btn'>Github</a>
+          <a href="https://github.com/mbarto202"  className='btn btn-primary'>Github</a>
+        </article>
+      </div>
     </section>
   )
 }
