25874c1af9d8fdfe622f3409c6ce4badfd850ed4
mbarto202 <michael.d.barto@gmail.com>
2022-09-15 13:54:24 -0400

    added to portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,20 +9,20 @@ const Portfolio = () => {
       <div className='container portfolio_container'>
         <article className='portfolio_item'>
           <div className='portfolio_img'></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/mbarto202"  className='btn' target='_blank'>Github</a>
+          <h3>Weather App</h3>
+          <a href="https://github.com/mbarto202/react-weather-app"  className='btn' target='_blank'>Github</a>
           <a href="https://github.com/mbarto202"  className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_img'></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/mbarto202"  className='btn' target='_blank'>Github</a>
+          <h3>Labyrinth</h3>
+          <a href="https://github.com/WCU-CSC476-F20/team-project-1-team-t/tree/master"  className='btn' target='_blank'>Github</a>
           <a href="https://github.com/mbarto202"  className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_img'></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/mbarto202"  className='btn' target='_blank'>Github</a>
+          <h3>Far Out Fairway</h3>
+          <a href="https://github.com/WCU-CSC476-F20/team-project-2-team-t"  className='btn' target='_blank'>Github</a>
           <a href="https://github.com/mbarto202"  className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
       </div>
