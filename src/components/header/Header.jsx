e6bdf59ae9c1e9e82591565980a4c72ac4dc288e
mbarto202 <michael.d.barto@gmail.com>
2024-11-26 23:45:58 -0500

    still trying to fix particles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,60 @@
 import React from "react";
+import { tsParticles } from "@tsparticles/engine";
+import Particles from "@tsparticles/react";
 import "./header.css";
-import Particles from "react-tsparticles";
 import CTA from "./CTA";
 import HeaderSocials from "./HeaderSocials";
-import { loadFull } from "tsparticles";
 
 const Header = () => {
-  const particlesInit = async (main) => {
-    console.log("Particles Loaded");
-    await loadFull(main);
+  const particlesInit = (engine) => {
+    console.log("Particles Init Called");
+    tsParticles.init(engine);
   };
 
   const particlesOptions = {
     particles: {
-      number: { value: 100 },
-      size: { value: 3 },
-      move: { enable: true, speed: 1 },
-      shape: { type: "circle" },
-      color: { value: "#fa8072" },
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#fa8072",
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: 3,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+      },
+    },
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+      },
     },
   };
 
   return (
     <header>
       <Particles
-        className="particles"
         id="tsparticles"
         init={particlesInit}
         options={particlesOptions}
+        className="particles"
       />
       <div className="container header__container">
         <h2 className="header__title">
