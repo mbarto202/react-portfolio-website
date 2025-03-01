194040660759f3c336d7ddca11df80d2c2a85827
mbarto202 <michael.d.barto@gmail.com>
2025-03-01 18:38:24 -0500

    epush

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import "./header.css"; // Ensure proper import
+import "./header.css";
 
 const HeaderSocials = () => {
   return (
