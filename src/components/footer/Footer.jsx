bc60539376f0d5687a83fd62166103b3f9f41c45
Michael <michael.d.barto@gmail.com>
2025-04-27 11:27:25 -0400

    updated resume

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
-import './footer.css'
-import {FaFacebookF} from 'react-icons/fa'
-import {BsLinkedin} from 'react-icons/bs'
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footerLogo'>Michael Barto</a>
+      <a href="#" className="footerLogo">
+        Michael Barto
+      </a>
 
-      <ul className='permalinks'>
-        <li><a href='#'>Home</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#portfolio'>Portfolio</a></li>
-        <li><a href='#contact'>Contact</a></li>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
       <div className="footerSocials">
-        <a href="https://linkedin.com"><BsLinkedin/></a>
-        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://linkedin.com">
+          <BsLinkedin />
+        </a>
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
       </div>
-    
+
       <div className="footerCopyright">
-        <small>&copy; Michael Barto. All rights reserved.</small>
+        <small>© 2025 Michael Barto. All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Resume from '../../assets/MIKE BARTO Resume.pdf'
+import React from "react";
+import Resume from "../../assets/resume draft.pdf";
 
 const CTA = () => {
   return (
-    <div className = 'cta'>
-        <a href = {Resume} download className='btn'>Download Resume</a>
-        <a href="#contact" className='btn btn-primary'>Contact Me</a>
+    <div className="cta">
+      <a href={Resume} download className="btn">
+        Download Resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Contact Me
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
